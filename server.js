658338673def195e3b226ed98035ff0aebd8ceaa
Harry <hfaulkner2006@gmail.com>
2026-03-12 13:48:32 +0000

    Create basic html page, write basic server.js code to describe / route

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+import express from "express";
+import "dotenv/config";
 
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("Running on http://localhost:3000"));
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html", {root: "public/html"});
+});
