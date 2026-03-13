27d1e507dee0d3ae081c76b10f320f41056a3fc8
Harry <hfaulkner2006@gmail.com>
2026-03-13 22:17:48 +0000

    Finalise design for login/signup pages, begin login/signup functionality

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Needed to get filepaths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Getting port from .env
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Middleware
+app.use(express.json()); // Allows the server to read JSON sent from your browser
+app.use(express.urlencoded({ extended: true })); // Allows server to read form data
+
 app.use(express.static("public"));
 
-app.listen(3000, () => console.log("Running on http://localhost:3000"));
-
+// --- ROUTES ---
 app.get("/", (req, res) => {
-	res.sendFile("index.html", {root: "public/html"});
+	res.sendFile("index.html", { root: path.join(__dirname, "public/html") });
 });
+
+app.get("/login", (req, res) => {
+	res.sendFile("login.html", { root: path.join(__dirname, "public/html") });
+});
+
+app.get("/signup", (req,res) => {
+	res.sendFile("signup.html", { root: path.join(__dirname, "public/html") });
+});
+
+app.listen(PORT, () => console.log("Running on http://localhost:3000"));
